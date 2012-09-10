cab32742ca3ef33e7e8b600a93467dcd6faf28bf
Fabrizio D'Ammassa <fabrizio.dammassa@gmail.com>
2012-09-10 11:07:11 +0200

    Added sql schema and reset script; added rsync script

---
+++ scripts/dms_synch_data.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+rsync -az --delete /home/fabrizio/Sites/dms/protected/uploads/ /home/fabrizio/Sites/noticeboard/protected/documents
---
+++ sql/reset.sql
@@ -0,0 +1,6 @@
+TRUNCATE TABLE documents;
+TRUNCATE TABLE spendings;
+TRUNCATE TABLE document_types;
+TRUNCATE TABLE entities;
+TRUNCATE TABLE proposer_services;
+TRUNCATE TABLE spending_offices;
