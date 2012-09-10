df0565807e266f57663eac20001c78e038d5b1f2
Fabrizio D'Ammassa <fabrizio.dammassa@gmail.com>
2012-09-10 11:35:27 +0200

    Fixed framework relative path and rsync script

--- scripts/dms_synch_data.bash
+++ scripts/dms_synch_data.bash
@@ -1,2 +1,3 @@
 #!/bin/bash
-rsync -az --delete /home/fabrizio/Sites/dms/protected/uploads/ /home/fabrizio/Sites/noticeboard/protected/documents
+rsync -az --delete /home/fabrizio/Sites/dms/protected/uploads/saved /home/fabrizio/Sites/noticeboard/protected/documents
+rsync -az --delete /home/fabrizio/Sites/dms/protected/uploads/spendings /home/fabrizio/Sites/noticeboard/protected/documents
