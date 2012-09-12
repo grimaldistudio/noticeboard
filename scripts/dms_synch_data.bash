cf04e505ada0b4a841ca13fde9b383d442ae2333
Fabrizio D'Ammassa <fabrizio.dammassa@gmail.com>
2012-09-12 15:26:31 +0200

    Updated rsync script

--- scripts/dms_synch_data.bash
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rsync -az --delete /home/fabrizio/Sites/dms/protected/uploads/{saved,spendings} /home/fabrizio/Sites/noticeboard/protected/documents
---
+++ scripts/rsynch.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rsync -az --delete --copy-dirlinks /home/fabrizio/Sites/dms/protected/uploads/public/ /home/fabrizio/Sites/noticeboard/protected/documents
