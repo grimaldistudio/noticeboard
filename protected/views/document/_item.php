225538d8d4c0edc16e04f23c173c4dcf203b7e59
Fabrizio D'Ammassa <fabrizio.dammassa@gmail.com>
2013-03-05 08:28:04 +0100

    Resized left column of item list

--- protected/views/document/_item.php
+++ protected/views/document/_item.php
@@ -1,7 +1,7 @@
 <table class="table table-striped table-condensed document-item-view detail-view">
 
     <tr>
-        <td style="width: 30%;">
+        <td style="width: 20%;">
             <label><?php echo Document::model()->getAttributeLabel('document_type_id'); ?></label>
         </td>
         <td>
@@ -10,42 +10,42 @@
     </tr>    
 
     <tr>
-        <td style="width: 30%;"><label><?php echo Document::model()->getAttributeLabel('protocol_number'); ?></label></td>
+        <td style="width: 20%;"><label><?php echo Document::model()->getAttributeLabel('protocol_number'); ?></label></td>
         <td><p><?php echo CHtml::encode($data->protocol_number!==null?$data->protocol_number:'n/d'); ?></p></td>
     </tr>
 
     <tr>
-        <td style="width: 30%;"><label><?php echo Document::model()->getAttributeLabel('publication_number'); ?></label></td>
+        <td style="width: 20%;"><label><?php echo Document::model()->getAttributeLabel('publication_number'); ?></label></td>
         <td><p><?php echo CHtml::encode($data->publication_number!==null?$data->publication_number:'n/d'); ?></p></td>
     </tr>    
     
     <tr>
-        <td style="width: 30%;"><label><?php echo Document::model()->getAttributeLabel('subject'); ?></label></td>
+        <td style="width: 20%;"><label><?php echo Document::model()->getAttributeLabel('subject'); ?></label></td>
         <td><p><?php echo CHtml::encode($data->subject); ?></p></td>
     </tr>
 
     <tr>
-        <td style="width: 30%;"><label><?php echo Document::model()->getAttributeLabel('act_number'); ?></label></td>
+        <td style="width: 20%;"><label><?php echo Document::model()->getAttributeLabel('act_number'); ?></label></td>
         <td><p><?php echo CHtml::encode($data->act_number?$data->act_number:'n/d'); ?></p></td>        
     </tr>
     
     <tr>
-        <td style="width: 30%;"><label><?php echo Document::model()->getAttributeLabel('entity_id'); ?></label></td>
+        <td style="width: 20%;"><label><?php echo Document::model()->getAttributeLabel('entity_id'); ?></label></td>
         <td><p><?php echo CHtml::encode($data->entity?$data->entity->name:Yii::app()->params['entity']); ?></p></td>
     </tr>
 
     <tr>
-        <td style="width: 30%;"><label><?php echo Document::model()->getAttributeLabel('proposer_service_id'); ?></label></td>
+        <td style="width: 20%;"><label><?php echo Document::model()->getAttributeLabel('proposer_service_id'); ?></label></td>
         <td><p><?php echo CHtml::encode($data->proposer_service?$data->proposer_service->name:'n/d'); ?></p></td>        
     </tr>
     
     <tr>
-        <td style="width: 30%;"><label><?php echo Document::model()->getAttributeLabel('publication_date_from'); ?></label></td>
+        <td style="width: 20%;"><label><?php echo Document::model()->getAttributeLabel('publication_date_from'); ?></label></td>
         <td><p><?php echo $data->publication_date_from?date('d-m-Y', strtotime($data->publication_date_from)):'n/d'; ?></p></td>
     </tr>
 
     <tr>
-        <td style="width: 30%;"><label><?php echo Document::model()->getAttributeLabel('publication_date_to'); ?></label></td>
+        <td style="width: 20%;"><label><?php echo Document::model()->getAttributeLabel('publication_date_to'); ?></label></td>
         <td><p><?php echo $data->publication_date_to?date('d-m-Y', strtotime($data->publication_date_to)):'n/d'; ?></p></td>        
     </tr>
     
--- protected/views/spending/_item.php
+++ protected/views/spending/_item.php
@@ -2,22 +2,22 @@
 <table class="table table-striped table-condensed document-item-view detail-view">
     
     <tr>
-        <td style="width: 30%"><label><?php echo $model->getAttributeLabel('title'); ?></label></td>
+        <td style="width: 20%"><label><?php echo $model->getAttributeLabel('title'); ?></label></td>
         <td><p><?php echo CHtml::encode($data->title); ?></p></td>
     </tr>
 
     <tr>
-        <td style="width: 30%"><label><?php echo $model->getAttributeLabel('receiver'); ?></label></td>
+        <td style="width: 20%"><label><?php echo $model->getAttributeLabel('receiver'); ?></label></td>
         <td><p><?php echo CHtml::encode($data->receiver); ?></p></td>        
     </tr>
     
     <tr>
-        <td style="width: 30%"><label><?php echo $model->getAttributeLabel('amount'); ?></label></td>
+        <td style="width: 20%"><label><?php echo $model->getAttributeLabel('amount'); ?></label></td>
         <td><p><?php echo CHtml::encode($data->amount)  ?> €</p></td>
     </tr>
 
     <tr>
-        <td style="width: 30%"><label><?php echo $model->getAttributeLabel('spending_date'); ?></label></td>
+        <td style="width: 20%"><label><?php echo $model->getAttributeLabel('spending_date'); ?></label></td>
         <td><p><?php echo $data->spending_date?date('d-m-Y', strtotime($data->spending_date)):'n/d'; ?></p></td>
     </tr>
     
