e0b3e1cd4ff424f59cc91771fef7e79932888542
Fabrizio D'Ammassa <fabrizio.dammassa@gmail.com>
2014-03-05 11:19:52 +0100

    Switched spendings list to grid view

--- protected/views/spending/list.php
+++ protected/views/spending/list.php
@@ -5,19 +5,25 @@ $this->pageTitle = "Elenco completo spese";
 
 <h1><?php echo $this->pageTitle; ?></h1>
 
-<?php $this->renderPartial('_advsearchform', array('model'=>$model)); ?>
-
-<?php
-
- $this->widget('bootstrap.widgets.BootListView', array(
-		'dataProvider'=> $model->search(),
-		'itemView'=>'_item',   // refers to the partial view named '_proficiency'
-                'template'=>"{sorter}\n{items}\n{pager}",        
-		'id' => 'spendingslistview',
-		'sortableAttributes'=>array(
-				'title',
-                                'spending_date'
-		),
-    ));
- ?>   
-
+<?php $this->widget('bootstrap.widgets.BootGridView', array(
+    'id'=>'spendings_gridview',
+    'dataProvider'=>$model->search(),
+    'template'=>"{items}\n{pager}",
+    'itemsCssClass'=>'table table-striped table-bordered table-condensed',
+    'columns'=>array(
+        array('name'=>'title'),
+        array('name'=>'receiver'),
+        array('name'=>'amount'),
+        array('name'=>'spending_date',
+             'filter'=>false,
+              'type' => 'datetime',
+              'value' => '$data->spending_date?strtotime($data->spending_date):\'n/a\'',
+             ),
+        array(
+            'class'=>'bootstrap.widgets.BootButtonColumn',
+            'htmlOptions'=>array('style'=>'width: 50px'),
+            'template'=>'{view}'
+        ),
+    ),
+    'filter'=>$model
+)); ?>
