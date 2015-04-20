e20b28edb732335b058eafae638b73c356bd3158
root <root@localhost>
2015-04-20 17:06:44 +0200

    sync

--- protected/models/Document.php
+++ protected/models/Document.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * This is the model class for table "document".
- *
- * The followings are the available columns in table 'document':
- * @property integer $num_pages
- *
- * @property Accessibilitymedia[] $accessibilitymedias
- */
-
-
 class Document extends CActiveRecord{
     
     const ENABLED = 1;
@@ -41,7 +31,7 @@ class Document extends CActiveRecord{
     public function rules()
     {
         return array(
-            array('subject,protocol_number,publication_number,document_type_id,entity_id,proposer_service_id,act_number,publication_date_from,publication_date_to,act_date_from,act_date_to', 'safe', 'on'=>'search'),
+            array('subject,protocol_number,publication_number,document_type_id,entity_id,proposer_service_id,act_number,publication_date_from,publication_date_to,num_pages,act_date_from,act_date_to', 'safe', 'on'=>'search'),
             array('publication_date_from,publication_date_to,act_date', 'default', 'value'=>new CDbExpression('NULL'), 'setOnEmpty'=>true, 'on'=>'create,update'),
             array('publication_date_from', 'date', 'format'=>'dd/MM/yyyy', 'timestampAttribute'=>'publication_date_from', 'allowEmpty'=>true, 'on'=>'search'),
             array('publication_date_to', 'date', 'format'=>'dd/MM/yyyy', 'timestampAttribute'=>'publication_date_to', 'allowEmpty'=>true, 'on'=>'search'),            
@@ -144,6 +134,7 @@ class Document extends CActiveRecord{
             $criteria->addCondition("act_date<='". date('Y-m-d H:i:s', $this->act_date_to)."'");        
         
         $criteria->compare('act_number', $this->act_number, true);
+        $criteria->compare('num_pages', $this->num_pages, true);
         $criteria->compare('subject',$this->subject,true);
         $criteria->compare('protocol_number', $this->protocol_number, true);
         $criteria->compare('publication_number', $this->publication_number, true);
