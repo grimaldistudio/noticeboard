5667639ee80369e39b89c5fc39a81e3216a9a0f2
root <root@localhost>
2015-04-20 11:53:41 +0200

    sync

--- protected/controllers/DocumentController.php
+++ protected/controllers/DocumentController.php
@@ -136,7 +136,7 @@ if (!Yii::app()->getRequest()->getIsAjaxRequest()) {
     
     public function actionView()
     {
-        $this->layout = "bootstrap";
+        //$this->layout = "bootstrap";
         $model = $this->loadModel();
         $this->render('view', array('model'=>$model));	        
     }
