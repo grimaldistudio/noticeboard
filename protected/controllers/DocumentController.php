1e61de8c0a405ac5e7ed5465e946d1e0943137c0
root <root@localhost>
2015-04-20 12:05:11 +0200

    sync

--- protected/controllers/DocumentController.php
+++ protected/controllers/DocumentController.php
@@ -52,7 +52,7 @@ class DocumentController extends Controller{
         array(
             'name'=>'publication_number',
             'type'=>'raw',
-            'value'=>'CHtml::link($data->publication_number,array("/noticeboard/document","id"=>$data->id))'
+            'value'=>'CHtml::ajaxLink($data->publication_number,array("noticeboard/document","id"=>$data->id), array("update"=>"#table"))'
             ),   
         array('name'=>'subject'),
       array('name'=>'document_type_id',
