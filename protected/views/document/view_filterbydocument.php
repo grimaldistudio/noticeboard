b843251410e31fa6b38bca1831939d895452d203
root <root@localhost>
2015-05-04 20:15:16 +0200

    New command options

--- protected/views/document/view_filterbydocument.php
+++ protected/views/document/view_filterbydocument.php
@@ -13,13 +13,13 @@
             'attributes'=>array(
                                 'name',
                                // 'protocol_number', 
-                              //  'act_number' => array('label'=>$model->getAttributeLabel('act_number'), 'value' => $model->identifier ) ,
+                                'act_number' => array('label'=>$model->getAttributeLabel('act_number'), 'value' => $model->identifier ) ,
                                 'act_date' => array('label'=>$model->getAttributeLabel('act_date'), 'type'=>'date', 'value' => $model->act_date?($model->act_date):null),
                                 'publication_date_from' => array('label'=>$model->getAttributeLabel('publication_date_from'), 'type'=>'datetime', 'value' => $model->publication_date_from?strtotime($model->publication_date_from):null),
                                 'publication_date_to' => array('label'=>$model->getAttributeLabel('publication_date_to'), 'type'=>'datetime', 'value' => $model->publication_date_to?strtotime($model->publication_date_to):null),
                                 'entity' => array('label'=>$model->getAttributeLabel('entity'), 'value' => $model->entity?$model->entity:Yii::app()->params['entity']),
                                 //'proposer_service_id' => array('label'=>$model->getAttributeLabel('proposer_service_id'), 'value' => $model->proposer_service?$model->proposer_service->name:'n/d'),
-                                'document_type' => array('label'=>$model->getAttributeLabel('document_type'), 'value' => $model->document_type?$data->getTypeDesc($data->document_type):'n/d'),
+                                'document_type' => array('label'=>$model->getAttributeLabel('document_type'), 'value' => $model->document_type?$model->getTypeDesc($data->document_type):'n/d'),
                                 'description' => array('label' => $model->getAttributeLabel('description'), 'type'=>'html', 'value'=>$model->description),
                                 'download' => array('label'=>'Documenti', 'type'=>'raw', 'value'=>'<a href="'.Yii::app()->createUrl('/document/download', array('id'=>$model->id)).'" target="_blank"><img src="'.Yii::app()->baseUrl.'/images/pdficon_large.png" /></a>  ('.$model->getDocumentSize().' KB)', 'visible'=>$model->sync_file==1)
                 ),
