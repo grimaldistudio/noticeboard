02e3d9703d51911535ede2d7f90cb1fc4daee860
root <root@localhost>
2015-05-05 09:05:18 +0200

    New command options

--- protected/controllers/DocumentController.php
+++ protected/controllers/DocumentController.php
@@ -294,8 +294,8 @@ if (!Yii::app()->getRequest()->getIsAjaxRequest()) {
     
       public function actionPreviewdoc($page = 0)
     {          
-        $model = $this->loadModel();
-die("here");    
+          die($_GET['id']);
+        $model = $this->loadModel();    
         $pm = new PreviewManager($model);
         header('Content-Type: image/jpeg');
         readfile($pm->getPreview(intval($page)));
