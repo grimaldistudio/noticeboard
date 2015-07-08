e4896bb76ae757ea393fa21432144b449d616d51
root <root@imacdidomenico.station>
2015-07-08 18:46:46 +0200

    product release

--- protected/controllers/DocumentController.php
+++ protected/controllers/DocumentController.php
@@ -66,7 +66,7 @@ class DocumentController extends Controller{
        array('name'=>'act_number',
              'header'=>'Protocollo/Atto e Data',
            'type'=>'raw',
-             'value'=>'($data->protocol_number)?$data->protocol_number."<br />Del: ".$data->act_date:$data->act_number."<br />Del: ".$data->act_date',
+             'value'=>'"N. ".($data->protocol_number)?$data->protocol_number."<br />del: ".$data->act_date:$data->act_number."<br />del: ".$data->act_date',
             'htmlOptions'=>array('width'=>'1%'),
            ),
       array('name'=>'document_type_id',
@@ -83,7 +83,7 @@ class DocumentController extends Controller{
                         array('name'=>'periodo',
                             'header'=> "Periodo esposizione",
                             'type'=>'raw',
-                            'value'=>'"Dal ".date("d/m/Y",strtotime($data->publication_date_from))."<br />Al ".$data->publication_date_to',
+                            'value'=>'"Dal ".date("d/m/Y",strtotime($data->publication_date_from))."<br />Al ".date("d/m/Y",strtotime($data->publication_date_to))',
                             )
    //     array('name'=>'proposer_service_id',
    //           'filter'=>CHtml::listData(ProposerService::model()->findAll(), 'id', 'name'),
